501322295ae667ac104835f88115a367175d39ff
Viktor Rura <ruravictor@gmail.com>
2024-06-13 15:21:17 +0100

    Added Inbox Page and Conversation component

---
+++ app/components/inbox/Conversation.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Conversation = () => {
+  return (
+    <div className="cursor-pointer px-6 py-4 border border-gray-300 rounded-xl">
+        <p className="mb-6 text-xl">John Doe</p>
+
+        <p className="text-airbnbDark">Go to conversation</p>
+
+
+    </div>
+  )
+}
+
+export default Conversation
--- app/myproperties/page.tsx
+++ app/myproperties/page.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-
+import PropertyList from "@/app/components/properties/PropertyList"
 const MyPropertiesPage = () => {
     return (
         <main className="max-w-[1500px] mx-auto px-6 pb-6">
             <h1 className="my-6 text-2xl">My Properties</h1>
+
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <PropertyList />
+            </div>
         </main>
     );
 };
