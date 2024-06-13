01cdbab5119bf695b47ab872cd3f697cddfe82a4
Viktor Rura <ruravictor@gmail.com>
2024-06-13 17:25:31 +0100

    Finished the Login Modal form

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -2,24 +2,43 @@
 import Modal from "./Modal";
 import { useState } from "react";
 import useLoginModal from "@/app/hooks/useLoginModal";
+import CustomButton from "../forms/CustomButton";
 
 const LoginModal = () => {
-const loginModal = useLoginModal();
+    const loginModal = useLoginModal();
 
-const content = (
-  <h2 className="mb-6 text-2xl">Welcome to DjangoBNB</h2>
-)
+    const content = (
+        <>
+            <form action="" className="space-y-4">
+                <input
+                    placeholder="Your email address"
+                    type="email"
+                    className="w-full h-[54px] px-4 border border-gray-300 rounded-xl"
+                />
+                <input
+                    placeholder="Your password"
+                    type="password"
+                    className="w-full h-[54px] px-4 border  border-gray-300 rounded-xl"
+                />
 
-  return (
-    <>
-      <Modal
-        isOpen={loginModal.isOpen}
-        close={loginModal.close}
-        content={content}
-        label="Log in"
-      />
-    </>
-  )
-}
+                <div className="p-5 bg-airbnb text-white rounded-xl opacity-80">Error message</div>
 
-export default LoginModal
+                <CustomButton
+                    label="Submit"
+                    onClick={() => console.log("Test")}
+                />
+            </form>
+        </>
+    );
+
+    return (
+        <Modal
+            isOpen={loginModal.isOpen}
+            close={loginModal.close}
+            content={content}
+            label="Log in"
+        />
+    );
+};
+
+export default LoginModal;
