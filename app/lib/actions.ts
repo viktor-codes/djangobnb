d72f06504281fc035d8c3ca346c573706955e405
Viktor Rura <ruravictor@gmail.com>
2024-06-15 08:54:26 +0100

    Implemented sighnup functionality. created actions.ts to store cookies.

---
+++ app/lib/actions.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+export async function handleLogin(
+    userId: string,
+    accessToken: string,
+    refreshToken: string
+) {
+    cookies().set("session_userid", userId, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 60 * 60 * 24 * 7, //One week
+        path: "/",
+    });
+    cookies().set("session_access_token", accessToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 60 * 60, //60 minutes
+        path: "/",
+    });
+    cookies().set("session_refresh_token", refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 60 * 60 * 24 * 7, //One week
+        path: "/",
+    });
+}
