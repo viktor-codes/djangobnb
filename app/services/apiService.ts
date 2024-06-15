1c9fccec3687d5f4d484419820631f8ad67b7242
Viktor Rura <ruravictor@gmail.com>
2024-06-15 07:53:03 +0100

    Added hanling the post method to the apiService file.

--- app/services/apiService.ts
+++ app/services/apiService.ts
@@ -20,7 +20,32 @@ const apiService = {
                     reject(error);
                 });
         })
+    },
+
+    post: async function (url: string, data: any): Promise<any> {
+        console.log('POST', url, data);
+
+        return new Promise ((resolve, reject) => {
+            fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
+                method: "POST",
+                body: data,
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+            })
+                .then((response) => response.json())
+                .then((json) => {
+                    console.log("Response:", json);
+
+                    resolve(json);
+                })
+                .catch((error) => {
+                    reject(error);
+                });
+        })
     }
+
 }
 
 export default apiService;
