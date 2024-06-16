60296acf6e9486aa641879aa2f6c438c09b85de6
Viktor Rura <ruravictor@gmail.com>
2024-06-16 14:12:17 +0100

    Added next button to the add property modal.

--- app/components/modals/AddPropertyModal.tsx
+++ app/components/modals/AddPropertyModal.tsx
@@ -3,12 +3,30 @@
 import Image from "next/image";
 
 import Modal from "./Modal";
-
+import { useState } from "react";
 import LoginModal from "./LoginModal";
 import useAddPropertyModal from "@/app/hooks/useAddPropertyModal";
+import CustomButton from "../forms/CustomButton";
 
 const AddPropertyModal = () => {
+    const [currentStep, setCurrentStep] = useState(1);
     const addPropertyModal = useAddPropertyModal();
+
+    const content = (
+        <>
+            {currentStep === 1 ? (
+                <>
+                <h2 className="mb-6 text-2xl"> Choose Category</h2>
+                <CustomButton label="next" onClick={() => setCurrentStep(2)} />
+                </>
+            ) : (
+                <p>step 2</p>
+            )
+        }
+            
+            
+        </>
+    )
 
     return (
         <>
@@ -16,9 +34,7 @@ const AddPropertyModal = () => {
                 isOpen={addPropertyModal.isOpen} 
                 close={addPropertyModal.close}
                 label="Add a new property"
-                content={(
-                    <p>You</p>
-                )}
+                content={content}
             />
         </>
     )
