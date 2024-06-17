7c0bcda16ec19b3e8c5edbaa9941947a815b822f
Viktor Rura <ruravictor@gmail.com>
2024-06-17 11:19:57 +0100

    Added handling mark as favorite for properties.

--- app/components/properties/PropertyList.tsx
+++ app/components/properties/PropertyList.tsx
@@ -10,6 +10,7 @@ export type PropertyType = {
     title: string;
     price_per_night: number;
     image_url: string;
+    is_favorite: boolean;
     
 };
 
@@ -21,6 +22,23 @@ interface PropertyListProps{
 const PropertyList: React.FC<PropertyListProps> = ({landlord_id}) => {
     const [properties, setProperties] = useState<PropertyType[]>([]);
 
+    const markFavorite = (id: string, is_favorite: boolean) => {
+        const tmpProperties = properties.map((property: PropertyType) => {
+            if (property.id == id) {
+                property.is_favorite = is_favorite;
+
+                if (is_favorite) {
+                    console.log("added to list of favorited propreties");
+                } else {
+                    console.log("removed from list");
+                }
+            }
+
+            return property;
+        });
+
+        setProperties(tmpProperties);
+    };
     const getProperties = async () => {
         let url = "/api/properties/";
 
@@ -30,7 +48,17 @@ const PropertyList: React.FC<PropertyListProps> = ({landlord_id}) => {
 
         const tmpProperties = await apiService.get(url);
         
-        setProperties(tmpProperties.data);
+        setProperties(
+            tmpProperties.data.map((property: PropertyType) => {
+                if (tmpProperties.favorites.includes(property.id)) {
+                    property.is_favorite = true;
+                } else {
+                    property.is_favorite = false;
+                }
+
+                return property;
+            })
+        );
     };
 
     useEffect(() => {
@@ -40,7 +68,13 @@ const PropertyList: React.FC<PropertyListProps> = ({landlord_id}) => {
         <>
             {properties.map((property) => {
                 return (
-                    <PropertyListItem key={property.id} property={property} />
+                    <PropertyListItem
+                        key={property.id}
+                        property={property}
+                        markFavorite={(is_favorite: any) =>
+                            markFavorite(property.id, is_favorite)
+                        }
+                    />
                 );
             })}
         </>
--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -1,20 +1,24 @@
-import React from "react";
 import Image from "next/image";
 import { PropertyType } from "./PropertyList";
 import { useRouter } from "next/navigation";
+import FavoriteButton from "../FavoriteButton";
 
 interface PropertyProps {
-    property: PropertyType;
+    property: PropertyType,
+    markFavorite?: (is_favorite: boolean) => void;
 }
 
-const PropertyListItem: React.FC<PropertyProps> = ({ property }) => {
+const PropertyListItem: React.FC<PropertyProps> = ({
+    property,
+    markFavorite
+}) => {
     const router = useRouter();
-
 
     return (
         <div 
-        onClick={() => router.push(`/properties/${property.id}`)}
-        className="cursor-pointer">
+            className="cursor-pointer"
+            onClick={() => router.push(`/properties/${property.id}`)}
+        >
             <div className="relative overflow-hidden aspect-square rounded-xl">
                 <Image
                     fill
@@ -23,17 +27,25 @@ const PropertyListItem: React.FC<PropertyProps> = ({ property }) => {
                     className="hover:scale-110 object-cover transition h-full w-full"
                     alt="Beach house"
                 />
+
+                {markFavorite && (
+                    <FavoriteButton
+                        id={property.id}
+                        is_favorite={property.is_favorite}
+                        markFavorite={(is_favorite) => markFavorite(is_favorite)}
+                    />
+                )}
             </div>
+
             <div className="mt-2">
                 <p className="text-lg font-bold">{property.title}</p>
             </div>
+
             <div className="mt-2">
-                <p className="text-sm text-gray-500">
-                    <strong>{property.price_per_night}</strong>
-                </p>
+                <p className="text-sm text-gray-500"><strong>${property.price_per_night}</strong> per night</p>
             </div>
         </div>
-    );
-};
+    )
+}
 
 export default PropertyListItem;
