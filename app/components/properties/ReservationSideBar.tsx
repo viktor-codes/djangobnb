1ea46f9019a2a4390cca8fb56b6747c1a0dcbd9d
Viktor Rura <ruravictor@gmail.com>
2024-06-17 08:54:02 +0100

    Added performBooking function to handle booking creation.

--- app/components/properties/ReservationSideBar.tsx
+++ app/components/properties/ReservationSideBar.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { Range } from "react-date-range";
 
-import { differenceInDays, eachDayOfInterval } from "date-fns";
+import { differenceInDays, eachDayOfInterval, format } from "date-fns";
 
 import apiService from "@/app/services/apiService";
 import useLoginModal from "@/app/hooks/useLoginModal";
@@ -13,21 +13,23 @@ const initialDateRange = {
     startDate: new Date(),
     endDate: new Date(),
     key: "selection",
-}
+};
 
 export type Property = {
-    id: string,
-    price_per_night: number,
-    guests: number,
-}
+    id: string;
+    price_per_night: number;
+    guests: number;
+};
 
 interface ReservationSideBarProps {
-    userId: string | null,
-    property: Property
+    userId: string | null;
+    property: Property;
 }
 
-const ReservationSideBar: React.FC<ReservationSideBarProps> = ({property, userId}) => {
-    
+const ReservationSideBar: React.FC<ReservationSideBarProps> = ({
+    property,
+    userId,
+}) => {
     const loginModal = useLoginModal();
     const [fee, setFee] = useState<number>(0);
     const [nights, setNights] = useState<number>(1);
@@ -35,9 +37,43 @@ const ReservationSideBar: React.FC<ReservationSideBarProps> = ({property, userId
     const [dateRange, setDateRange] = useState<Range>(initialDateRange);
     const [minDate, setMinDate] = useState<Date>(new Date());
     const [guests, setGuests] = useState<string>("1");
-    const guestsRange = Array.from({length: property.guests}, (_, i) => (i + 1));
+    const guestsRange = Array.from(
+        { length: property.guests },
+        (_, i) => i + 1
+    );
 
-    const _setDateRange =(selection: any) => {
+    const performBooking = async () => {
+        if (userId) {
+            if (dateRange.startDate && dateRange.endDate) {
+                const formData = new FormData();
+                formData.append("guests", guests);
+                formData.append(
+                    "start_date",
+                    format(dateRange.startDate, "yyyy-MM-dd")
+                );
+                formData.append(
+                    "end_date",
+                    format(dateRange.endDate, "yyyy-MM-dd")
+                );
+                formData.append("number_of_nights", nights.toString());
+                formData.append("total_price", totalPrice.toString());
+
+                const response = await apiService.post(
+                    `/api/properties/${property.id}/book/`,
+                    formData
+                );
+                if (response.success) {
+                    console.log("Booking successful");
+                } else {
+                    console.log("Booking failed");
+                }
+            }
+        } else {
+            loginModal.open();
+        }
+    };
+
+    const _setDateRange = (selection: any) => {
         const newStartDate = new Date(selection.startDate);
         const newEndDate = new Date(selection.endDate);
 
@@ -49,17 +85,19 @@ const ReservationSideBar: React.FC<ReservationSideBarProps> = ({property, userId
             ...dateRange,
             startDate: newStartDate,
             endDate: newEndDate,
-        })
-    }
+        });
+    };
 
-
-    useEffect(() =>{
+    useEffect(() => {
         if (dateRange.startDate && dateRange.endDate) {
-            const dayCount = differenceInDays(dateRange.endDate, dateRange.startDate);
+            const dayCount = differenceInDays(
+                dateRange.endDate,
+                dateRange.startDate
+            );
             if (dayCount && property.price_per_night) {
-                const _fee = ((dayCount * property.price_per_night) / 100) * 5; 
+                const _fee = ((dayCount * property.price_per_night) / 100) * 5;
                 setFee(_fee);
-                setTotalPrice((dayCount * property.price_per_night) + _fee);
+                setTotalPrice(dayCount * property.price_per_night + _fee);
                 setNights(dayCount);
             } else {
                 const _fee = (property.price_per_night / 100) * 5;
@@ -69,7 +107,7 @@ const ReservationSideBar: React.FC<ReservationSideBarProps> = ({property, userId
                 setNights(1);
             }
         }
-    }, [dateRange])
+    }, [dateRange]);
 
     return (
         <aside className="mt-6 p-6 col-span-2 rounded-xl border border-gray-300 shadow-xl">
@@ -100,7 +138,10 @@ const ReservationSideBar: React.FC<ReservationSideBarProps> = ({property, userId
                 </select>
             </div>
 
-            <div className="cursor-pointer w-full mb-6 py-6 text-center text-white bg-airbnb hover:bg-airbnb-dark rounded-xl">
+            <div
+                onClick={performBooking}
+                className="cursor-pointer w-full mb-6 py-6 text-center text-white bg-airbnb hover:bg-airbnb-dark rounded-xl"
+            >
                 Book
             </div>
 
@@ -108,9 +149,7 @@ const ReservationSideBar: React.FC<ReservationSideBarProps> = ({property, userId
                 <p>
                     {property.price_per_night} * {nights} nights
                 </p>
-                <p>
-                    ${property.price_per_night * nights}
-                </p>
+                <p>${property.price_per_night * nights}</p>
             </div>
             <div className="mt-4 flex justify-between alighn-center">
                 <p>DjangoBnb fee</p>
@@ -125,4 +164,4 @@ const ReservationSideBar: React.FC<ReservationSideBarProps> = ({property, userId
     );
 };
 
-export default ReservationSideBar
+export default ReservationSideBar;
