901585ad64ba68820caf66c7e040172db974b4b1
Viktor Rura <ruravictor@gmail.com>
2024-06-18 14:31:01 +0100

    Added inbox page and made it dynamic.

--- app/components/inbox/Conversation.tsx
+++ app/components/inbox/Conversation.tsx
@@ -1,15 +1,32 @@
-import React from 'react'
+"use client";
 
-const Conversation = () => {
-  return (
-    <div className="cursor-pointer px-6 py-4 border border-gray-300 rounded-xl">
-        <p className="mb-6 text-xl">John Doe</p>
+import { useRouter } from "next/navigation";
+import { ConversationType } from "@/app/inbox/page";
 
-        <p className="text-airbnbDark">Go to conversation</p>
-
-
-    </div>
-  )
+interface ConversationProps {
+    conversation: ConversationType;
+    userId: string;
 }
 
-export default Conversation
+const Conversation: React.FC<ConversationProps> = ({
+    conversation,
+    userId,
+}) => {
+    const router = useRouter();
+    const otherUser = conversation.users.find((user) => user.id != userId);
+
+    return (
+        <div className="px-6 py-4 cursor-pointer border border-gray-300 rounded-xl">
+            <p className="mb-6 text-xl">{otherUser?.name}</p>
+
+            <p
+                onClick={() => router.push(`/inbox/${conversation.id}`)}
+                className="text-airbnb-dark"
+            >
+                Go to conversation
+            </p>
+        </div>
+    );
+};
+
+export default Conversation;
