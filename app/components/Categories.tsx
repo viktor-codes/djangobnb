e0c68a20d3637e6b9af8df38a9521119fa044146
Viktor Rura <ruravictor@gmail.com>
2024-06-18 18:59:48 +0100

    Implemented filtering properties by category.

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -1,9 +1,53 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
+import useSearchModal, { SearchQuery } from "../hooks/useSearchModal";
+
 const Categories = () => {
+    const searchModal = useSearchModal();
+    const [category, setCategory] = useState("");
+
+    const _setCategory = (_category: string) => {
+        setCategory(_category);
+
+        const query: SearchQuery = {
+            country: searchModal.query.country,
+            checkIn: searchModal.query.checkIn,
+            checkOut: searchModal.query.checkOut,
+            guests: searchModal.query.guests,
+            bedrooms: searchModal.query.bedrooms,
+            bathrooms: searchModal.query.bathrooms,
+            category: _category,
+        };
+
+        searchModal.setQuery(query);
+    };
+
     return (
         <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
-            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+            <div
+                onClick={() => _setCategory("")}
+                className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
+                    category == "" ? "border-black" : "border-white"
+                } opacity-60 hover:border-gray-200 hover:opacity-100`}
+            >
+                <Image
+                    src="/icn_category_beach.jpeg"
+                    alt="Category - Beach"
+                    width={20}
+                    height={20}
+                />
+
+                <span className="text-xs">All</span>
+            </div>
+
+            <div
+                onClick={() => _setCategory("beach")}
+                className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
+                    category == "beach" ? "border-black" : "border-white"
+                } opacity-60 hover:border-gray-200 hover:opacity-100`}
+            >
                 <Image
                     src="/icn_category_beach.jpeg"
                     alt="Category - Beach"
@@ -13,35 +57,53 @@ const Categories = () => {
 
                 <span className="text-xs">Beach</span>
             </div>
-            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+
+            <div
+                onClick={() => _setCategory("villas")}
+                className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
+                    category == "villas" ? "border-black" : "border-white"
+                } opacity-60 hover:border-gray-200 hover:opacity-100`}
+            >
                 <Image
                     src="/icn_category_beach.jpeg"
-                    alt="Category - Villas"
+                    alt="Category - Beach"
                     width={20}
                     height={20}
                 />
 
                 <span className="text-xs">Villas</span>
             </div>
-            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+
+            <div
+                onClick={() => _setCategory("cabins")}
+                className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
+                    category == "cabins" ? "border-black" : "border-white"
+                } opacity-60 hover:border-gray-200 hover:opacity-100`}
+            >
                 <Image
                     src="/icn_category_beach.jpeg"
-                    alt="Category - Cabinas"
+                    alt="Category - Beach"
                     width={20}
                     height={20}
                 />
 
-                <span className="text-xs">Cabinas</span>
+                <span className="text-xs">Cabins</span>
             </div>
-            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+
+            <div
+                onClick={() => _setCategory("tiny_homes")}
+                className={`pb-4 flex flex-col items-center space-y-2 border-b-2 ${
+                    category == "tiny_homes" ? "border-black" : "border-white"
+                } opacity-60 hover:border-gray-200 hover:opacity-100`}
+            >
                 <Image
                     src="/icn_category_beach.jpeg"
-                    alt="Category - Tiny House"
+                    alt="Category - Beach"
                     width={20}
                     height={20}
                 />
 
-                <span className="text-xs">Tiny House</span>
+                <span className="text-xs">Tiny homes</span>
             </div>
         </div>
     );
