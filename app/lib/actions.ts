8ef4ec3da7cae3e3e64d5b95b7b59d70be059058
Viktor Rura <ruravictor@gmail.com>
2024-06-18 19:21:40 +0100

    Updated actions.ts to handle token refresh.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,47 @@
 
 import { cookies } from "next/headers";
 
+export async function handleRefresh() {
+    console.log("handleRefresh");
+
+    const refreshToken = await getRefreshToken();
+
+    const token = await fetch("http://localhost:8000/api/auth/token/refresh/", {
+        method: "POST",
+        body: JSON.stringify({
+            refresh: refreshToken,
+        }),
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+    })
+        .then((response) => response.json())
+        .then((json) => {
+            console.log("Response - Refresh:", json);
+
+            if (json.access) {
+                cookies().set("session_access_token", json.access, {
+                    httpOnly: true,
+                    secure: false,
+                    maxAge: 60 * 60, // 60 minutes
+                    path: "/",
+                });
+
+                return json.access;
+            } else {
+                resetAuthCookies();
+            }
+        })
+        .catch((error) => {
+            console.log("error", error);
+
+            resetAuthCookies();
+        });
+
+    return token;
+}
+
 export async function handleLogin(
     userId: string,
     accessToken: string,
@@ -9,26 +50,28 @@ export async function handleLogin(
 ) {
     cookies().set("session_userid", userId, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        maxAge: 60 * 60 * 24 * 7, //One week
+        secure: false,
+        maxAge: 60 * 60 * 24 * 7, // One week
         path: "/",
     });
+
     cookies().set("session_access_token", accessToken, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        maxAge: 60 * 60, //60 minutes
+        secure: false,
+        maxAge: 60 * 60, // 60 minutes
         path: "/",
     });
+
     cookies().set("session_refresh_token", refreshToken, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        maxAge: 60 * 60 * 24 * 7, //One week
+        secure: false,
+        maxAge: 60 * 60 * 24 * 7, // One week
         path: "/",
     });
 }
 
 export async function resetAuthCookies() {
-    cookies().set("session_userid", "")
+    cookies().set("session_userid", "");
     cookies().set("session_access_token", "");
     cookies().set("session_refresh_token", "");
 }
@@ -43,5 +86,16 @@ export async function getUserId() {
 
 export async function getAccessToken() {
     let accessToken = cookies().get("session_access_token")?.value;
+
+    if (!accessToken) {
+        accessToken = await handleRefresh();
+    }
+
     return accessToken;
+}
+
+export async function getRefreshToken() {
+    let refreshToken = cookies().get("session_refresh_token")?.value;
+
+    return refreshToken;
 }
